7b2c82bdf4114798df6c1d1ebc0a9466d3b8f298
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2022-03-19 18:34:15 +0700

    aaa

--- baovietpj/src/components/HomeComponents/Header.js
+++ baovietpj/src/components/HomeComponents/Header.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo_header from '../../stlye/img/logo_header.png'
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
+import { BiDownArrow } from 'react-icons/bi'
 function Header() {
+    const profileicon = {
+        color: "white", fontSize: "1.2em"
+    }
+    const arrow = {
+        color: "white", fontSize: ".8em"
+    }
+    const [showlist, setShowlist] = useState(false)
+    const toggleshowlist = () => {
+        if (showlist == false) {
+            setShowlist(true)
+        } else {
+            setShowlist(false)
+        }
+        console.log(showlist);
+    }
+    const Accountdropdown = () => {
+        return (
+            <div className='account-dropdownlist'>
+                <div className='wrap-pane-setting'>
+                    <div className='setting-box'>
+                        <div className='wrap-setting'>
+                            <h5>Giao diện</h5>
+                            <div className='setting-pane'>
+                                <div className='setting-option'>
+                                    <input type='radio' />
+                                    <span>Sáng</span>
+                                </div>
+                                <div className='setting-option'>
+                                    <input type='radio' />
+                                    <span>Tối</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='setting-box'>
+                        <div className='wrap-setting'>
+                            <h5>Ngôn ngữ</h5>
+                            <div className='setting-pane'>
+                                <div className='setting-option'>
+                                    <input type='radio' />
+                                    <span>Việt Nam</span>
+                                </div>
+                                <div className='setting-option'>
+                                    <input type='radio' />
+                                    <span>Tiếng Anh</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <header>
@@ -11,9 +66,14 @@ function Header() {
                     <img className='logo_header' src={logo_header} />
                     <div className='log-in'>
                         <Link to="/"><button className='log-in-btn'>Đăng xuất</button></Link>
+                        <div className='wrapp'>
+                            <div className='toggle-info' onClick={toggleshowlist}>
+                                <CgProfile style={profileicon} /><BiDownArrow style={arrow} />
+                            </div>
+                        </div>
                     </div>
-                    
                 </div>
+                <div>  {showlist ? <Accountdropdown /> : ''}</div>
             </header>
         </div>
     )
