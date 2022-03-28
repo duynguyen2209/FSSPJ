75d8fdf4f7873e7c78701d8ed879c084f9626117
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2022-03-28 11:54:07 +0700

    ss

--- baovietpj/src/App.js
+++ baovietpj/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Login from '../src/components/Login/Login';
 import { Routes, Route} from 'react-router-dom';
 import ShowHome from './components/PriceBoard/HomeRoute';
-import NotFound from './components/PriceBoard/NotFound';
 function App() {
     return (
         <>
             <Routes>
                 <Route exact path="/" element={<Login />} />
-                <Route path={'/Home'} element={<ShowHome/>}/>
-                <Route element={<NotFound/>}/>
+                <Route path={'/Home'} element={<ShowHome/>}/>S
             </Routes>
         </>
     );
