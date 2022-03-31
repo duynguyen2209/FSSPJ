cdc8ba09d0bee250a66195f2ef0af0f1b1be4548
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2022-03-31 12:38:39 +0700

    .

--- baovietpj/src/i18next.js
+++ baovietpj/src/i18next.js
@@ -1,10 +1,8 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
-import English from './locales/eng/translation.json';
-import Vienames from './locales/vie/translation.json';
-// the translations
-const Languages = ['vie', 'eng']
+import English from './locales/eng/transEng.json';
+import Vienames from './locales/vie/transVie.json';
 
 i18n
     .use(Backend)
@@ -20,7 +18,6 @@ i18n
         },
         fallbackLng: 'vie',
         debug: true,
-        whitelist:Languages,
     });
 
 export default i18n;
