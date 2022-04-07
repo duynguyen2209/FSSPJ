4760e33beaacde31c4dc759cf64af2e66c12a7c7
Nguyen Duc DUy <72897050+duynguyen2209@users.noreply.github.com>
2022-04-07 11:35:23 +0700

    ::

--- baovietpj/src/components/PriceBoard/Body/Table/TableBody.js
+++ baovietpj/src/components/PriceBoard/Body/Table/TableBody.js
@@ -1,3 +1,6 @@
+import React, { useState } from 'react'
+import './InfoCompany.scss'
+
 export const CaculatePB = (item, percent, fix) => {
     if (item !== 0 && item !== undefined && item !== null && item !== NaN) {
         return ((Math.round(item * 1) / percent).toFixed(fix))
@@ -61,40 +64,59 @@ export const highlight = (currentValue, value, item) => {
     }
 }
 
+
+
 export function TableView(item) {
+    const [checked, setChecked] = useState(false)
+    function InfoCompany(item) {
+        return (
+            <div className='infmodalBg'>
+                <div className='Modalinfo-box'>
+                    <br/>
+                    <h1>Thông tin mã chứng khoán</h1>
+                    <button className='btn-close' onClick={() => setChecked(!checked)}>[ X ]</button>
+                    <div className='Showhidetab'>
+                        <h2>{item.title}</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
-      <>
-                  <tbody>
-                      <tr>
-                          <td className={setColor(item, item.closePrice)} scope="row" title={item.FullName}>{item.symbol}</td>
-                          <td className="td-thamchieu">{CaculatePB(item.reference, 1000, 2)}</td>
-                          <td className="td-tran">{CaculatePB(item.ceiling, 1000, 2)}</td>
-                          <td className="td-san">{CaculatePB(item.floor, 1000, 2)}</td>
-                          <td className={setColor(item,item.bidPrice3)}>{CaculatePB(item.bidPrice3, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.bidPrice3, item.bidVol3)}>{CaculatePB(item.bidVol3, 1000, 2)}</td>
-                          <td className={setColor(item, item.bidPrice2)}>{CaculatePB(item.bidPrice2, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.bidPrice2, item.bidVol2)}>{CaculatePB(item.bidVol2, 1000, 2)}</td>
-                          <td className={setColor(item, item.bidPrice1)}>{CaculatePB(item.bidPrice1, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.bidPrice1, item.bidVol1)}>{CaculatePB(item.bidVol1, 1000, 2)}</td>
-                          <td className={setColor(item, item.closePrice)}>{CaculatePB(item.closePrice, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.closePrice)}>{CaculatePB(item.closeVol, 10, 0)}</td>
-                          <td className={setColorBeside(item, item.closePrice)}>{CaculatorPercent(item.changePercent)} {item.changePercent ? '%' : ''}</td>
-                          <td className={setColor(item, item.offerPrice1)}>{CaculatePB(item.offerPrice1, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.offerPrice1)}>{CaculatePB(item.offerVol1, 1000, 2)}</td>
-                          <td className={setColor(item, item.offerPrice2)}>{CaculatePB(item.offerPrice2, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.offerPrice2)}>{CaculatePB(item.offerVol2, 1000, 2)}</td>
-                          <td className={setColor(item, item.offerPrice3)}>{CaculatePB(item.offerPrice3, 1000, 2)}</td>
-                          <td className={setColorBeside(item, item.offerPrice3)}>{CaculatePB(item.offerVol3, 1000, 2)}</td>
-                          <td className="noneChangeColor">{CaculatePB(item.totalTrading, 1000, 2)}</td>
-                          <td className={setColor(item, item.high)}>{CaculatePB(item.high, 1000, 2)}</td>
-                          <td className={setColor(item, item.averagePrice)}>{CaculatePB(item.averagePrice, 1000, 2)}</td>
-                          <td className={setColor(item, item.low)}>{CaculatePB(item.low, 1000, 2)}</td>
-                          <td className="noneChangeColor">{ }</td>
-                          <td className="noneChangeColor">{ }</td>
-                          <td className="noneChangeColor">{CaculatePB(item.foreignBuy, 1000, 2)}</td>
-                          <td className="noneChangeColor">{CaculatePB(item.foreignSell, 1000, 2)}</td>
-                      </tr>
-                  </tbody>
-              </>
+        <>
+            <tbody>
+                <tr>
+                    <td className={setColor(item, item.closePrice)} onClick={()=>setChecked(true)} scope="row" title={item.FullName}>{item.symbol}</td>
+                    {checked && <InfoCompany />}
+                    <td className="td-thamchieu">{CaculatePB(item.reference, 1000, 2)}</td>
+                    <td className="td-tran">{CaculatePB(item.ceiling, 1000, 2)}</td>
+                    <td className="td-san">{CaculatePB(item.floor, 1000, 2)}</td>
+                    <td className={setColor(item, item.bidPrice3)}>{CaculatePB(item.bidPrice3, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.bidPrice3, item.bidVol3)}>{CaculatePB(item.bidVol3, 1000, 2)}</td>
+                    <td className={setColor(item, item.bidPrice2)}>{CaculatePB(item.bidPrice2, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.bidPrice2, item.bidVol2)}>{CaculatePB(item.bidVol2, 1000, 2)}</td>
+                    <td className={setColor(item, item.bidPrice1)}>{CaculatePB(item.bidPrice1, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.bidPrice1, item.bidVol1)}>{CaculatePB(item.bidVol1, 1000, 2)}</td>
+                    <td className={setColor(item, item.closePrice)}>{CaculatePB(item.closePrice, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.closePrice)}>{CaculatePB(item.closeVol, 10, 0)}</td>
+                    <td className={setColorBeside(item, item.closePrice)}>{CaculatorPercent(item.changePercent)} {item.changePercent ? '%' : ''}</td>
+                    <td className={setColor(item, item.offerPrice1)}>{CaculatePB(item.offerPrice1, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.offerPrice1)}>{CaculatePB(item.offerVol1, 1000, 2)}</td>
+                    <td className={setColor(item, item.offerPrice2)}>{CaculatePB(item.offerPrice2, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.offerPrice2)}>{CaculatePB(item.offerVol2, 1000, 2)}</td>
+                    <td className={setColor(item, item.offerPrice3)}>{CaculatePB(item.offerPrice3, 1000, 2)}</td>
+                    <td className={setColorBeside(item, item.offerPrice3)}>{CaculatePB(item.offerVol3, 1000, 2)}</td>
+                    <td className="noneChangeColor">{CaculatePB(item.totalTrading, 1000, 2)}</td>
+                    <td className={setColor(item, item.high)}>{CaculatePB(item.high, 1000, 2)}</td>
+                    <td className={setColor(item, item.averagePrice)}>{CaculatePB(item.averagePrice, 1000, 2)}</td>
+                    <td className={setColor(item, item.low)}>{CaculatePB(item.low, 1000, 2)}</td>
+                    <td className="noneChangeColor">{ }</td>
+                    <td className="noneChangeColor">{ }</td>
+                    <td className="noneChangeColor">{CaculatePB(item.foreignBuy, 1000, 2)}</td>
+                    <td className="noneChangeColor">{CaculatePB(item.foreignSell, 1000, 2)}</td>
+                </tr>
+            </tbody>
+        </>
     )
-  }
+}
+
